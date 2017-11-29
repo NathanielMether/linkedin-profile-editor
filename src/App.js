a98049d21443d7a22fe8922f6aa2895d8459adc3
NathanielMether <nmet3@student.monash.edu>
2017-11-29 12:01:52 +1100

    Add editing to user attributes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,42 @@ class App extends Component {
     }
   }
 
+  onChangeFirstName = (event) => {
+    const input = event.target
+    const newFirstName = input.value
+    this.setState((prevState) => {
+      const user = prevState.user
+      const newUser = { ...user, firstName: newFirstName}
+      return {
+        user: newUser
+      }
+    })
+  }
+
+  onChangeLastName = (event) => {
+    const input = event.target
+    const newLastName = input.value
+    this.setState((prevState) => {
+      const user = prevState.user
+      const newUser = { ...user, lastName: newLastName}
+      return {
+        user: newUser
+      }
+    })
+  }
+
+  onChangeImage = (event) => {
+    const input = event.target
+    const newImage = input.value
+    this.setState((prevState) => {
+      const user = prevState.user
+      const newUser = { ...user, profileImageURL: newImage}
+      return {
+        user: newUser
+      }
+    })
+  }
+
   render() {
     const user = this.state.user
 
@@ -18,6 +54,29 @@ class App extends Component {
         <h1>LinkedIn Profile Editor</h1>
         <img src={user.profileImageURL}/>
         <p>Name: {user.firstName } {user.lastName}</p>
+        <label>
+          First name:
+          {' '}
+          <input value={ user.firstName }
+            onChange={ this.onChangeFirstName }
+          />
+        </label>
+        <label>
+          Last name:
+          {' '}
+          <input value={ user.lastName }
+            onChange={ this.onChangeLastName }
+          />
+        </label>
+        <div>
+          <label>
+            Image URL:
+            {' '}
+            <input value={ user.profileImageURL }
+              onChange={ this.onChangeImage }
+            />
+          </label>
+        </div>
       </div>
     );
   }
