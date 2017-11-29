12b22728087acdbb00c12ec6174bfe8a2c8e8d88
NathanielMether <nmet3@student.monash.edu>
2017-11-29 11:16:50 +1100

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+### Models
+
+1. Render out hard coded data
+2. Model your data (similar to process in Rails)
+3. Add state yo your app
+4. Make render use app's state (remove hard-coding from render)
+5. Handle events from interactive elements (`onClick`, `onChange`)
+6. Connect those to methods that update the app's state (`this.setState`)
+
+#### USer
+-first name 
+-last name
+-profile image
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    user: {
+      firstName: 'Alice',
+      lastName: 'Jones',
+      profileImageURL: 'https://randomuser.me/api/portraits/women/50.jpg'
+    }
+  }
+
+  render() {
+    const user = this.state.user
+
+    return (
+      <div className="App">
+        <h1>LinkedIn Profile Editor</h1>
+        <img src={user.profileImageURL}/>
+        <p>Name: {user.firstName } {user.lastName}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
